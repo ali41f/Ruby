13c22d979e388a13a86571eb4f62e8fa36acc94c
Antimony51122 <zhuyaosheng51122@gmail.com>
2017-09-11 12:06:50 +0100

    new tic tac toe project on progress

--- Udemy-tutorials/Tic_Tac_Toe/main.rb
+++ Udemy-tutorials/Tic_Tac_Toe/main.rb
@@ -7,3 +7,11 @@ players = [
 ]
 
 p game = Game.new(players)
+
+puts 'Welcome to tic tac toe.'
+puts "it is player one's turn"
+puts 'Player one, pick a move'
+
+# TODO: Get a move from player one
+# TODO: Mark that place on the board
+# TODO: Check for win
